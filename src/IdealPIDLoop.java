3788ec009e8d68ac76f240539cd22378b882063b
Sahit Chintalapudi <chsahit@gmail.com>
2016-08-01 07:56:46 -0400

    ideal pid loop now has a full pid loop w a lookback

--- src/IdealPIDLoop.java
+++ src/IdealPIDLoop.java
@@ -1,20 +1,50 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class IdealPIDLoop {
 	float kP, kI, kD;
 	float setpoint;
-	float thistime, lasttime;
-	float error;
+	float lasttime = -1, lasterror;
+	float error, netError = 0, dError;
+	Queue<Float> lookback; 
 	public IdealPIDLoop(float kP, float kI, float kD, float sp) {
 		this.kP = kP;
 		this.kI = kI;
 		this.kD = kD;
 		this.setpoint = sp;
+		lasterror = setpoint;
+		lookback = new LinkedList<Float>();
 	}
 	
 	public float getOutput(float position, float time) {
-		for(int i = 0; i < 800; i++) {
-			error = setpoint - position; 
-		}		
-		return (error * kP);
+		error = setpoint - position;
+		netError += ((time - lasttime) * error);
+		dError = ((error - lasterror)/(time - lasttime));
+		System.out.println(dError + " " + (time - lasttime));
+		lasttime = time;
+		lasterror = error;
+		return ((error * kP) + (netError * kI) + (dError * kD));
+	}
+	
+	public float getOutputWithLookback(float position, float time) {
+		error = setpoint - position;
+		lookback.add((time - lasttime) * error);
+		if (lookback.size() > 100) {
+			lookback.poll();
+		}
+		netError = sum(lookback);
+		dError = ((error - lasterror)/(time - lasttime));
+		System.out.println(dError + " " + (time - lasttime));
+		lasttime = time;
+		lasterror = error;
+		return ((error * kP) + (netError * kI) + (dError * kD));
+	}
+	
+	private float sum(Queue<Float> list) {
+		float sum = 0;
+		for (Float f: list) {
+			sum += f;
+		}
+		return sum;
 	}
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -2,28 +2,19 @@ public class Simulator {
 
 	public static void main(String[] args) {
 		Robot robot = new Robot();		
+		IdealPIDLoop pid = new IdealPIDLoop(0.1f, 0.0004f, 3f, 40);
 		Logger.init();
-		testPLoop(robot);
+		float output;
+		for(int i = 0; i < 4200; i++) {
+			output = pid.getOutput(robot.getPosition(), i);
+			robot.set(output);
+			Logger.writeData(output + "," + robot.getPosition());
+			robot.step();
+			
+		}
 		Logger.close();
 		System.out.println("END");
 		System.out.println(robot.robot.getLinearVelocity().x);
-		//System.out.println(robot.floor.getContactList().toString());
-	}
-	
-	public static void testPLoop(Robot robot) {
-		float setpoint = 40;
-		float error = 0, kP = 0.2f;
-		for(int i = 0; i < 800; i++) {
-			error = setpoint - robot.getPosition();
-			robot.set(error * kP);
-			robot.step();
-			Logger.writeData(robot.getPosition());
-			System.out.println(robot.robot.getPosition().x + " " + robot.robot.getPosition().y);
-		}		
-	}
-	
-	public static void testStaticF(Robot robot) {
-		
 	}
 
 }
