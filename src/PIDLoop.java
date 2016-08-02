3001b9c5719a444fb70fd3c13bfd43af385de17d
Sahit Chintalapudi <chsahit@gmail.com>
2016-08-02 14:21:06 -0400

    added dubious std pid loop to be debugged, i isnt accumulating

--- src/PIDLoop.java
+++ src/PIDLoop.java
@@ -1,13 +1,13 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class IdealPIDLoop {
+public class PIDLoop {
 	float kP, kI, kD;
 	float setpoint;
 	float lasttime = -1, lasterror;
 	float error, netError = 0, dError;
 	Queue<Float> lookback; 
-	public IdealPIDLoop(float kP, float kI, float kD, float sp) {
+	public PIDLoop(float kP, float kI, float kD, float sp) {
 		this.kP = kP;
 		this.kI = kI;
 		this.kD = kD;
@@ -16,7 +16,7 @@ public class IdealPIDLoop {
 		lookback = new LinkedList<Float>();
 	}
 	
-	public float getOutput(float position, float time) {
+	public float getIdealOutput(float position, float time) {
 		error = setpoint - position;
 		netError += ((time - lasttime) * error);
 		dError = ((error - lasterror)/(time - lasttime));
@@ -26,7 +26,7 @@ public class IdealPIDLoop {
 		return ((error * kP) + (netError * kI) + (dError * kD));
 	}
 	
-	public float getOutputWithLookback(float position, float time) {
+	public float getIdealOutputWithLookback(float position, float time) {
 		error = setpoint - position;
 		lookback.add((time - lasttime) * error);
 		if (lookback.size() > 300) {
@@ -40,6 +40,27 @@ public class IdealPIDLoop {
 		Logger.writeData((error * kP) + "," + (netError * kI) + "," + 
 				(dError * kD) + "," + position);
 		return ((error * kP) + (netError * kI) + (dError * kD));
+	}
+	
+	public float getStandardOutput(float position, float time) {
+		float error = setpoint - position;
+		if(position < 0.8 * setpoint) {
+			Logger.writeData((kP * error) + ",0,0," + position);
+			return (kP * error);
+		} else {
+			lookback.add((time - lasttime) * error);
+			if (lookback.size() > 500) {
+				lookback.poll();
+			}
+			netError = sum(lookback);
+			dError = ((error - lasterror)/(time - lasttime));
+			lasttime = time;
+			lasterror = error;
+			Logger.writeData(0 + "," + (netError * kI) + "," + 
+					(dError * kD) + "," + position);
+			return ((netError * kI) + (dError * kD));
+		}
+		
 	}
 	
 	private float sum(Queue<Float> list) {
--- src/Simulator.java
+++ src/Simulator.java
@@ -2,11 +2,12 @@ public class Simulator {
 
 	public static void main(String[] args) {
 		Robot robot = new Robot();		
-		IdealPIDLoop pid = new IdealPIDLoop(0.2f, 0.001f, 3f, 40);
+		PIDLoop pidIdeal = new PIDLoop(0.2f, 0.001f, 3f, 40);
+		PIDLoop pidStd = new PIDLoop(0.2f, 0.0015f, 3f, 40);
 		Logger.init();
 		float output;
 		for(int i = 0; i < 4200; i++) {
-			output = pid.getOutputWithLookback(robot.getPosition(), i);
+			output = pidStd.getStandardOutput(robot.getPosition(), i);
 			robot.set(output);
 			robot.step();
 			
